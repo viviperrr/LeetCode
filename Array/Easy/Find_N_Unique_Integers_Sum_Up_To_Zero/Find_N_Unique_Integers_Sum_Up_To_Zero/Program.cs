bf4765c990fe75d6e29b6465f4a903bb1409c2cb
Kyle Kim <kyle.kim@amwestfunding.com>
2020-08-28 14:18:01 -0700

    Added Array/Easy/ Find n unique integers sum up to zero

---
+++ Array/Easy/Find_N_Unique_Integers_Sum_Up_To_Zero/Find_N_Unique_Integers_Sum_Up_To_Zero/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Find_N_Unique_Integers_Sum_Up_To_Zero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            int[] result = SumZero(n);
+
+            Console.Write("[");
+            foreach (var r in result)
+            {
+                Console.Write("{0}, ", r);
+            }
+            Console.Write("]");
+        }
+
+        /// <summary>
+        /// Time Complexity: O(N) input: n
+        /// Space Comlexity: O(N) input: n
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+
+        static int[] SumZero(int n)
+        {
+            int[] result = new int[n];
+            int index = n / 2;
+
+            if (n == 1)
+            {
+                result[0] = 0;
+                return result;
+            }
+
+            while (n > index)
+            {
+                result[n - 1] = n;
+                result[result.Length - n] = -n;
+                n--;
+            }
+
+            if (result.Length % 2 == 0)
+            {
+                result[n - 1] = n;
+                result[result.Length - n] = -n;
+            }
+            else
+            {
+                result[n] = 0;
+            }
+
+            return result;
+        }
+    }
+}
