db5fd79abd064b9c7936be5f214eb07cbe5161e6
Kyle Kim <kyle.kim@amwestfunding.com>
2020-09-10 14:46:25 -0700

    Range Sum of BST

---
+++ Tree/Easy/Range_Sum_BST/README.md
@@ -0,0 +1,24 @@
+## 938. Range Sum of BST
+
+Link: https://leetcode.com/problems/range-sum-of-bst/
+
+**Description**
+Given the root node of a binary search tree, return the sum of values of all nodes with value between L and R (inclusive).
+
+The binary search tree is guaranteed to have unique values.
+
+**For example:**
+Input: root = [10,5,15,3,7,null,18], L = 7, R = 15
+Output: 32
+Example 2:
+
+Input: root = [10,5,15,3,7,13,18,1,null,6], L = 6, R = 10
+Output: 23
+
+**Result**
+return [2].
+
+**Note:**
+
+- The number of nodes in the tree is at most 10000.
+- The final answer is guaranteed to be less than 2^31.
---
+++ Tree/Easy/Range_Sum_BST/Range_Sum_BST/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Range_Sum_BST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            
+            int l = 7;
+            int r = 15;
+
+            TreeNode node = new TreeNode(10);
+            node.left = new TreeNode(5);
+            node.left.left = new TreeNode(3);
+            node.left.right = new TreeNode(7);
+            node.right = new TreeNode(18);
+            node.right.left = new TreeNode(15);
+
+            int sum = solution.RangeSumBST(node, l, r);
+            Console.WriteLine(sum);
+        }
+    }
+
+    public class Solution
+    {
+        public int RangeSumBST(TreeNode root, int L, int R)
+        {
+            int sum = 0;
+            if (root == null)
+            {
+                return sum;
+            }
+
+            InorderTraverser(root, L, R, ref sum);
+
+            return sum;
+        }
+
+        /// <summary>
+        /// Time Complexity: O(n)
+        /// Space Complexity: O(1)
+        /// </summary>
+        /// <param name="sum"></param>
+        private void InorderTraverser(TreeNode root, int l, int r, ref int sum)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            InorderTraverser(root.left, l, r, ref sum);
+
+            if (root.val >= l && root.val <= r)
+            {
+                sum += root.val;
+            }
+
+            InorderTraverser(root.right, l, r, ref sum);
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
